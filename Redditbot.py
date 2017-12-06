5d5b1c1d891874ed4e4e990f8ee4cfe0e28a7ed5
P1nxy <34229148+P1nxy@users.noreply.github.com>
2017-12-06 10:59:39 +0200

    Add files via upload

--- Redditbot.py
+++ Redditbot.py
@@ -1,9 +1,29 @@
 import praw
+import time
 
-r = praw.reddit(user_agent = "Schoolbot by Marek /u/P3nxy")
-r.login()
+r = praw.reddit(user_agent = "Schoolbot by Marek /u/Marki_Bot")#redditi useri nimi
+print("Sisse logimine...")
+r.login()#siia saab lisada logini, et ei peaks koguaeg sisse logima aga see ei ole turvaline kui programmi sheerida
+
+words_to_match = ['tee siia list mis sõnadele, mida ta kontrollib!!']
+cache = [] #siia storetakse kõik kommentid
 
 def run_bot():
-    subreddit = r.get subreddit("test")
-    comment = subreddit.get_comments(limit=25)
+    print("sisenen subredditisse...")
+    subreddit = r.get_subreddit("test/koolibot") #thredi nimi, vaata üle seda. testi.
+    print("Vaatan komentaare...")
+    comment = subreddit.get_comments(limit=25)#et bot ei overloadiks redditi serveri
     for comment in comments:
+        comment_text = comment.body.lower()#Kui see matchi sõna on suuretähega kirjutatud siis ikkagi saaks aru sellest ja võtaks seda kui matchina
+        isMatch = any(string in comment_text for string in words_to_match)
+        if comment.id not in cache and isMatch:#et ta ei vastaks koguaeg samale komentaarile
+            ("Match on leitud! komentaari ID:" + comment.id)
+            comment.reply(' Siia komm mida bot vastab!!')
+            print("Vastus õnnestus!")
+            cache.append(comment.id)
+    print("Üks loop tehtud aeg on magada..")
+            
+
+while true:
+    run_bot()
+    time.sleep(10)#cooldown time
